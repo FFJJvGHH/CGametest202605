e503ac4e8afd66c2c0765de605e7dfddadfbba0f
Ailu <3780167695@qq.com>
2026-05-21 14:10:48 +0800

    save testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,27 @@ int main()
 	std::cout << "new test" << std::endl;
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(1080, 640, "game");
+
+	rlImGuiSetup(true);
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		ClearBackground(BLACK);
+		ClearBackground(RAYWHITE);
 		DrawText("OPEN Window", 190, 200, 20, RED);
 
+		rlImGuiBegin();
+
+
+		ImGui::Begin("test");
+		ImGui::Text("Hello");
+		ImGui::End();
+
+		rlImGuiEnd();
 		EndDrawing();
 	}
 
+	rlImGuiShutdown();
+	CloseWindow();
+	
 	return 0;
 }
