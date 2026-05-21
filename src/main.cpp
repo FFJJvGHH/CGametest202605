70e292038ee8e6ab0aa466545748166b7a7ba714
Ailu <3780167695@qq.com>
2026-05-21 13:58:10 +0800

    add includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
 	std::cout << "Hello\n";
 	std::cout << "new test" << std::endl;
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1080, 640, "game");
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawText("OPEN Window", 190, 200, 20, RED);
+
+		EndDrawing();
+	}
+
 	return 0;
 }
